b45f1e90c6b36c70549b9dedeb8db96e1df1b3d9
msow1 <you@example.com>
2019-06-26 17:10:56 -0400

    Adding new demo for p0

---
+++ beans/CarBuilder.java
@@ -0,0 +1,32 @@
+package com.mj.beans;
+
+	public class CarBuilder {
+		
+		public static void main(String[] args) {
+
+	
+		   Cars c1 = new Cars();
+		
+			c1.setMake("Toyata");
+			c1.setModel("Camri");
+			c1.setMileage(1000);
+			c1.setYear(2016);
+			c1.setPrice(12000);
+			
+			Cars c2 = new Cars();
+			c2.setMake("Hunday");
+			c2.setModel("Civic");
+			c2.setMileage(50000);
+			c2.setYear(2014);
+			c2.setPrice(10000);
+		
+			Cars c3 = new Cars();
+			c3.setMake("Peugeo");
+			c3.setModel("Macarina");
+			c3.setMileage(0);
+			c3.setYear(2019);
+			c3.setPrice(35000);
+		}
+			
+
+}
