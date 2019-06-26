443462efbdf5d33270b19335958985ab1bbb5875
msow1 <you@example.com>
2019-06-26 17:12:09 -0400

    Adding the start p0

---
+++ beans/Cars.java
@@ -0,0 +1,54 @@
+package com.mj.beans;
+
+public class Cars {
+	
+	private String VIN;
+	private String make;
+	private String model;
+	private int year;
+	private float price;
+	private int mileage;
+	
+	
+	
+	
+	public String getVIN() {
+		return VIN;
+	}
+	public void setVIN(String vIN) {
+		VIN = vIN;
+	}
+
+
+	public int getMileage() {
+		return mileage;
+	}
+	public void setMileage(int milage) {
+		this.mileage = milage;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+}
